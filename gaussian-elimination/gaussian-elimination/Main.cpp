54a033a96523c37b918fced2281863b8d3ae49ba
benjoboy <serec.benjamin@gmail.com>
2023-01-07 17:45:35 +0100

    fix

--- gaussian-elimination/gaussian-elimination/Main.cpp
+++ gaussian-elimination/gaussian-elimination/Main.cpp
@@ -9,11 +9,8 @@ int main(int argc, char** argv) {
 	//cout << sizeof(float) * 8 << "   " << sizeof(double)*8 << endl;
 	//solver.printMatrix();
 	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < 10; i++)
-	{
-		solver.readMatix("test2.txt");
-		solver.gaussElimination();
-	}
+	solver.readMatix("test2.txt");
+	solver.gaussElimination();
 	auto stop = std::chrono::high_resolution_clock::now();
 
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
@@ -26,7 +23,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
-
-//  1 1				test3 01
-//
